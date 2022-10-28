5393e15979c231126e57a7fa68d76bb392fdb5e4
phvleite <phvleite@gmail.com>
2022-10-28 19:15:56 -0300

    feat: implementada a função test_relatorio_produto() em test_product_report.py

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.factories.product_factory import ProductFactory
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    np = ProductFactory()
+    prd = Product(
+        np.id,
+        np.nome_do_produto,
+        np.nome_da_empresa,
+        np.data_de_fabricacao,
+        np.data_de_validade,
+        np.numero_de_serie,
+        np.instrucoes_de_armazenamento,
+    )
+    result = str(prd.__repr__)
+    assert prd.nome_do_produto in result
+    assert prd.nome_da_empresa in result
+    assert prd.data_de_fabricacao in result
+    assert prd.data_de_validade in result
+    assert prd.instrucoes_de_armazenamento in result
