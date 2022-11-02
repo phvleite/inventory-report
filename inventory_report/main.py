e84c63d284df60dc6fddd4ee7b3a3fd02f0f1875
phvleite <phvleite@gmail.com>
2022-11-02 10:31:35 -0300

    style: alteração para teste manual da classe CsvImporter()

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,51 +1,54 @@
-from inventory_report.inventory.product import Product
-from tests.factories.product_factory import ProductFactory
-from inventory_report.reports.simple_report import SimpleReport
-from inventory_report.reports.complete_report import CompleteReport
-from inventory_report.inventory.inventory import Inventory
+# from inventory_report.inventory.product import Product
+# from tests.factories.product_factory import ProductFactory
+# from inventory_report.reports.simple_report import SimpleReport
+# from inventory_report.reports.complete_report import CompleteReport
+# from inventory_report.inventory.inventory import Inventory
+from inventory_report.importer.csv_importer import CsvImporter
 
 
-def product_test():
-    np = ProductFactory()
-    produto = Product(
-        np.id,
-        np.nome_do_produto,
-        np.nome_da_empresa,
-        np.data_de_fabricacao,
-        np.data_de_validade,
-        np.numero_de_serie,
-        np.instrucoes_de_armazenamento,
-    )
-    return {
-        "id": produto.id,
-        "nome_do_produto": produto.nome_do_produto,
-        "nome_da_empresa": produto.nome_da_empresa,
-        "data_de_fabricacao": produto.data_de_fabricacao,
-        "data_de_validade": produto.data_de_validade,
-        "numero_de_serie": produto.numero_de_serie,
-        "instrucoes_de_armazenamento": produto.instrucoes_de_armazenamento,
-    }
+# def product_test():
+#     np = ProductFactory()
+#     produto = Product(
+#         np.id,
+#         np.nome_do_produto,
+#         np.nome_da_empresa,
+#         np.data_de_fabricacao,
+#         np.data_de_validade,
+#         np.numero_de_serie,
+#         np.instrucoes_de_armazenamento,
+#     )
+#     return {
+#         "id": produto.id,
+#         "nome_do_produto": produto.nome_do_produto,
+#         "nome_da_empresa": produto.nome_da_empresa,
+#         "data_de_fabricacao": produto.data_de_fabricacao,
+#         "data_de_validade": produto.data_de_validade,
+#         "numero_de_serie": produto.numero_de_serie,
+#         "instrucoes_de_armazenamento": produto.instrucoes_de_armazenamento,
+#     }
 
 
 def main():
-    data_list = []
-    for i in range(11):
-        prd = product_test()
-        data_list.append(prd)
+    # data_list = []
+    # for i in range(11):
+    #     prd = product_test()
+    #     data_list.append(prd)
 
-    print("SimpleReport:")
-    print(SimpleReport.generate(data_list))
-    print("\n\n")
-    print("CompleteReport:")
-    print(CompleteReport.generate(data_list))
-    print("\n\n")
-    print("Inventory")
-    print(
-        Inventory.import_data(
-            "inventory_report/data/inventory.xml",
-            "simples",
-        )
-    )
+    # print("SimpleReport:")
+    # print(SimpleReport.generate(data_list))
+    # print("\n\n")
+    # print("CompleteReport:")
+    # print(CompleteReport.generate(data_list))
+    # print("\n\n")
+    # print("Inventory")
+    # print(
+    #     Inventory.import_data(
+    #         "inventory_report/data/inventory.xml",
+    #         "simples",
+    #     )
+    # )
+    print("CsvImporter:")
+    print(CsvImporter.import_data("inventory_report/data/inventory.csv"))
 
 
 if __name__ == "__main__":
