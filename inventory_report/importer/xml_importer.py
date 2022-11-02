9f9585286baa2c24366ea0715b4432a8fbd58fb2
phvleite <phvleite@gmail.com>
2022-11-02 11:09:26 -0300

    fix: removido a camada para analise de existência do arquivo, ficando como primeira camada de segurança a verificação do tijpo de arquivo.

---
+++ inventory_report/importer/xml_importer.py
@@ -0,0 +1,19 @@
+from inventory_report.importer.importer import Importer
+import xmltodict
+
+
+class XmlImporter(Importer):
+    @classmethod
+    def import_data(cls, filepath: str):
+        if filepath.endswith(".xml"):
+            data = XmlImporter.__read_file(filepath)
+        else:
+            raise ValueError("Arquivo inválido")
+        return data
+
+    def __read_file(filepath):
+        with open(filepath, "r") as xml_file:
+            inventory_reader = xmltodict.parse(xml_file.read())["dataset"][
+                "record"
+            ]
+        return inventory_reader
