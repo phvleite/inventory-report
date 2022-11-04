0f022af4c6c6b6b9d812c2961152d0e5fae2ae4e
phvleite <phvleite@gmail.com>
2022-11-04 12:01:45 -0300

    fix: correção implementação método iteratos

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -4,12 +4,12 @@ import csv
 
 class CsvImporter(Importer):
     @classmethod
-    def import_data(cls, filepath: str):
+    def import_data(cls, filepath: str, type: str):
         if filepath.endswith(".csv"):
             data = CsvImporter.__read_file(filepath)
         else:
             raise ValueError("Arquivo inválido")
-        return data
+        return data, type
 
     def __read_file(filepath):
         with open(filepath, encoding="utf8", mode="r") as csv_file:
